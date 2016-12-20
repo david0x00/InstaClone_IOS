07cc50097527dc61ca37c2a89e15311ee686bd8f
dnull7 <null2@illinois.edu>
2016-12-19 21:11:12 -0600

    first commit for instagram clone. Xcode project created

---
+++ instragram/instragram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instragram
+//
+//  Created by David Null on 12/19/16.
+//  Copyright © 2016 David Null. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
